de12c1269654f043208f9a5a4e84c38b3fef87f2
FantasticApple <zhawhjw@users.noreply.github.com>
2025-02-17 22:56:47 -0800

    first submission

--- application/app.py
+++ application/app.py
@@ -1,9 +1,10 @@
 from flask import Flask
+from application.bp.homepage import bp_homepage
 
 # initialize Flask service
 app = Flask(__name__)
 # register blueprint
-
+app.register_blueprint(bp_homepage)
 
 if __name__ == "__main__":
     app.run(debug=True)
